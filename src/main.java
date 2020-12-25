202df376e299a849bce2d9e50a6c836486db2aac
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-25 14:33:10 -0300

    001

--- src/main.java
+++ src/main.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class main {
     public static void main(String[] args) {
         var point = new Point(1,2);
@@ -5,5 +7,28 @@ public class main {
         var val = InitializeExample.ps[0];
         TLC tlc = new TLC();
         TLC.SMC smc = new TLC.SMC();
+        Iterator seq = suffixes("teste");
+        while (seq.hasNext())
+            System.out.println(seq.next());
+    }
+    static Iterator suffixes(final String s){
+//        class SuffixIterator implements  Iterator{
+//            int i =0;
+//            public boolean hasNext(){return i<s.length();}
+//            public Object next(){return s.substring(i++);}
+//        }
+//        return new SuffixIterator();
+        return new Iterator() {
+            int i =0;
+            @Override
+            public boolean hasNext() {
+                return i<s.length();
+            }
+
+            @Override
+            public Object next() {
+                return s.substring(i++);
+            }
+        };
     }
 }
