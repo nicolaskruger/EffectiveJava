232ca85c5be7f2850732a66e32273433003c1aa6
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-27 13:42:44 -0300

    001

--- src/main.java
+++ src/main.java
@@ -8,42 +8,36 @@ import java.awt.*;
 
 public class main extends Canvas {
 
-    public static void main(String[] args) throws Exception {
-        buffer();
-    }
-    public static void buffer(){
-        Buffer buf = new Buffer();
-        Thread t1 = new Thread(()->{
-           System.out.println(buf.get());
-        });
-        Thread t2 = new Thread(()->{
-           buf.put(3);
-        });
-        t1.start();
-        t2.start();
-    }
-    public static void bank(){
-        Bank bank = new Bank();
-        new Clerk(bank).start();
-        new Clerk(bank).start();
-    }
-    public static void  print(){
-        Printer[] print = {
-                new Printer(),
-                new Printer(),
+    public static  void STBuffer(){
+        String str[] = {
+                "hello",
+                " word",
+                " ola",
+                " mundo"
         };
-        for (Printer printer : print) {
-            printer.start();
+        StringBuffer res = new StringBuffer();
+        for (String s : str) {
+            res.append(s);
+            System.out.println(res.toString());
         }
+    }
+    public static String CL(){
+        String s = "elefant";
+        StringBuffer res = new StringBuffer();
+        char cl = 'e';
+        String s2 = "E";
+        for (int i=0;i<s.length();i++){
+            if(s.charAt(i)==cl){
+                res.append(s2);
+            }else {
+                res.append(s.charAt(i));
+            }
+        }
+        return res.toString();
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println(CL());
+    }
 
-    }
-    public static void incement(){
-        Incrementer u = new Incrementer();
-        u.start();
-        for (;;){
-//            System.in.read();
-            System.out.println(u.i);
-        }
-    }
 
 }
