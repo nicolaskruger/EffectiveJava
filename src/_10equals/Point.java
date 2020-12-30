7493df506a39d4ffb969754d4bcbf15947412395
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-12-30 16:56:29 -0300

    001

---
+++ src/_10equals/Point.java
@@ -0,0 +1,16 @@
+package _10equals;
+
+public class Point {
+    private final int x;
+    private final int y;
+    public Point(int x,int y) {
+        this.x = x;
+        this.y = y;
+    }
+    @Override public boolean equals(Object o){
+        if(!(o instanceof  Point))
+            return false;
+        Point p = (Point) o;
+        return p.x ==x &&p.y==y;
+    }
+}
