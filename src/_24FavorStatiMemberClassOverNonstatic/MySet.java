467e765700399add3addab8688a350108ebfb723
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-01-02 20:45:34 -0300

    001

---
+++ src/_24FavorStatiMemberClassOverNonstatic/MySet.java
@@ -0,0 +1,27 @@
+package _24FavorStatiMemberClassOverNonstatic;
+
+import java.util.AbstractSet;
+import java.util.Iterator;
+
+public class MySet <E> extends AbstractSet<E> {
+    @Override
+    public Iterator<E> iterator() {
+        return new MyIterator();
+    }
+    private class MyIterator implements Iterator<E>{
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public E next() {
+            return null;
+        }
+    }
+    @Override
+    public int size() {
+        return 0;
+    }
+}
